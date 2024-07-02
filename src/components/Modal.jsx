60c787c6b2a623faa7bd0d266ad4574082705c3f
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-02 15:38:30 +0200

    mise en place du systeme de connexion

---
+++ src/components/Modal.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Button from "./Button";
+
+const Modal = ({ isOpen, onClose, onSubmit }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ email, password });
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-90">
+      <div className="bg-white p-12 rounded-lg w-[30vw]">
+        <h2 className="mb-8">Connexion</h2>
+        <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Mot de passe
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="flex justify-end gap-3">
+            <Button text="Annuler" color="danger" onClick={onClose} />
+            <Button text="Valider" color="dark" type="submit" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- src/shared/endpoints.js
+++ src/shared/endpoints.js
@@ -1,3 +1,5 @@
 const REACT_APP_API_BASE_URL = "http://127.0.0.1:3000";
 
 export const getProjects = REACT_APP_API_BASE_URL + "/api/projects/";
+
+export const connexionUrl = REACT_APP_API_BASE_URL + "/api/users/login/";
