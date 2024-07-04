4e4d003e43cbc93782b592dc877d5f884234a1c0
ThorKild_ <pro.antoine.masia@gmail.com>
2024-07-04 15:21:28 +0200

    mise en place du bouton delete et de l'animation

---
+++ src/components/ButtonDelete.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ButtonDelete = () => {
+  return (
+    <img
+      className="size-12 absolute -right-3 -top-3 animate-shake cursor-pointer"
+      src="/src/assets/images/trash.png"
+    />
+  );
+};
+
+export default ButtonDelete;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        shake: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "25%": { transform: "rotate(-5deg)" },
+          "50%": { transform: "rotate(5deg)" },
+          "75%": { transform: "rotate(-5deg)" },
+        },
+      },
+      animation: {
+        shake: "shake 1s infinite",
+      },
       colors: {
         yellow: {
           light: "#fefcbf",
